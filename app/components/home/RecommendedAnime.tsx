248ef0cf0e9fc02250cf2ba368989fa034fbe7fd
Hein Thu Aung <heinthu92001@gmail.com>
2024-10-09 22:30:54 +0100

    make responsive

--- app/components/home/RecommendedAnime.tsx
+++ app/components/home/RecommendedAnime.tsx
@@ -13,9 +13,9 @@ const RecommendedAnime = ({ animes }: { animes: NewEpisode }) => {
   return (
     <div className="flex flex-col justify-center items-center">
       <h2 className="font-bold text-3xl mr-auto ml-10 my-10">Watch New Episodes</h2>
-      <div className="grid grid-cols-5 mx-auto px-10 items-center justify-items-center gap-3 mb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 mx-auto px-10 items-center justify-items-center gap-3 mb-10">
         {currentItems.map((anime) => (
-          <div key={anime?.entry?.mal_id} className="col-span-1 h-[420px] ">
+          <div key={anime?.entry?.mal_id} className="col-span-1 h-[420px] w-[250px]">
             <img
               src={anime?.entry?.images?.jpg?.image_url || " "}
               alt={anime?.entry?.title || ""}
