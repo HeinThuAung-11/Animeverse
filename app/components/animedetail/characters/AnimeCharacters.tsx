66bc3a9214fb20403577669013767605459aca6f
Hein Thu Aung <heinthu92001@gmail.com>
2024-10-25 11:20:12 +0100

    fix error

--- app/components/animedetail/characters/AnimeCharacters.tsx
+++ app/components/animedetail/characters/AnimeCharacters.tsx
@@ -6,7 +6,7 @@ const animeCharacters = ({ characters }: { characters?: Characters }) => {
   return (
     <>
       <h2 className="text-white text-3xl font-bold mt-6">Characters</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 3xl:grid-cols-4 gap-4">
         {characters?.data?.map(({ character, voice_actors, role }) => {
           return <CharacterCard character={character} voice_actors={voice_actors} role={role} key={character.mal_id} />;
         })}
--- app/components/animedetail/characters/CharacterCard.tsx
+++ app/components/animedetail/characters/CharacterCard.tsx
@@ -3,27 +3,32 @@ export const CharacterCard = ({ character, voice_actors, role }: CharacterCard) 
     <div
       key={character.mal_id}
       className="flex items-center bg-[#1A1B20] rounded-md border border-gray-100 hover:border-2 hover:border-lime-500 transition duration-300">
-      <div className="flex items-center w-[200px]">
+      {/* Left side: Character Image and Info */}
+      <div className="flex items-center w-1/2 space-x-4">
         <img
           src={character?.images?.jpg?.image_url}
           alt={character.name}
-          className="w-32 h-32 object-cover flex-1 rounded-md"
+          className="w-32 h-32 object-cover rounded-md"
         />
-        <div className="flex flex-col justify-between items-center flex-1">
-          <h3 className="text-white text-md font-semibold text-center">{character.name}</h3>
+        <div className="flex flex-col justify-center items-center w-32 text-center">
+          <h3 className="text-white text-md font-semibold">{character.name}</h3>
           <h3 className="text-white text-md">{role}</h3>
         </div>
       </div>
+
+      {/* Divider */}
       <div className="border-l-2 border-gray-600 h-32"></div>
-      <div className="flex items-center w-[200px] flex-1">
-        <div className="flex flex-col justify-evenly items-center">
-          <h3 className="text-white text-md font-semibold text-center"> {voice_actors[0]?.person?.name}</h3>
+
+      {/* Right side: Voice Actor Image and Info */}
+      <div className="flex items-center w-1/2 space-x-4">
+        <div className="flex flex-col justify-center items-center w-32 text-center">
+          <h3 className="text-white text-md font-semibold">{voice_actors[0]?.person?.name}</h3>
           <h3 className="text-white text-md">{voice_actors[0]?.language}</h3>
         </div>
         <img
           src={voice_actors[0]?.person?.images?.jpg?.image_url}
           alt={character.name}
-          className="w-32 h-32 object-cover flex-1 rounded-md"
+          className="w-32 h-32 object-cover rounded-md"
         />
       </div>
     </div>
